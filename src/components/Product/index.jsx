a6d93984406e4f928bdc6bbd28e94785aa08c646
SreckoStojic <stojic.srecko@gmail.com>
2021-11-10 14:52:58 +0100

    Change a tag to Link.

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,9 +1,10 @@
 import styles from './Product.module.css';
+import { Link } from 'react-router-dom';
 
 function Product({product, handleClick}) {
     return (
         <div className={styles['product']}>
-            <a href={`/products/product/${product.id}`} ><img alt={product.name} src={product.imgUrl} /></a>
+            <Link to={`/products/product/${product.id}`} ><img alt={product.name} src={product.imgUrl} /></Link>
             <h2>{product.name}</h2>
             <h1>{product.price} RSD</h1>
             <button type="button" onClick={handleClick}>Add To Cart</button>
